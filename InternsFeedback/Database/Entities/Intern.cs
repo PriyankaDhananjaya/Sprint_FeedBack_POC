def9f90919e7a11e6a08f3350c1dd94f19877d99
Priyanka Dhananjaya <Priyanka.Dhananjaya@neudesic.com>
2020-04-13 14:16:49 +0530

    intern and project files added.

---
+++ InternsFeedback/Database/Entities/Intern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InternsFeedback.Database.Entities
+{
+    public partial class Intern
+    {
+        public Intern()
+        {
+            Feedback = new HashSet<Feedback>();
+        }
+
+        public int InternId { get; set; }
+        public string InternName { get; set; }
+        public int CreatedBy { get; set; }
+        public int ProjectId { get; set; }
+        public virtual User CreatedByNavigation { get; set; }
+        public virtual Project Project { get; set; }
+        public virtual ICollection<Feedback> Feedback { get; set; }
+    }
+}
---
+++ InternsFeedback/Database/Entities/Project.cs
@@ -0,0 +1,21 @@
+﻿using InternsFeedback.Database.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace InternsFeedback.Database.Entities
+{
+    public partial class Project
+    {
+        public Project()
+        {
+            Intern = new HashSet<Intern>();
+        }
+
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+        public virtual ICollection<Intern> Intern { get; set; }
+    }
+}
